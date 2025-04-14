eb37ea3131323c75b172eca17c356959cf5314d3
andyiac <andyiaczhang@gmail.com>
2025-04-14 10:44:24 +0800

    Update InterfaceDefault.java

--- interface/InterfaceDefault.java
+++ interface/InterfaceDefault.java
@@ -1,16 +1,10 @@
-
-
 /**
-
 Java 8 新增了接口的默认方法。
 
 简单说，默认方法就是接口可以有实现方法，而且不需要实现类去实现其方法。
 
 我们只需在方法名前面加个 default 关键字即可实现默认方法。
-
 */
-
-
 class InterfaceDefault implements Duck {
 	@Override
 	public void kua() {
@@ -19,18 +13,12 @@ class InterfaceDefault implements Duck {
 
 	public static void main(String[] args) {
 		new InterfaceDefault().kua();
-		
 	}
 }
 
 interface Duck {
 	void kua();
 	default void egg() {
-
 		System.out.println("hello from interface ");
-
 	}
-	default void testDefault() {
-		
-	};
 }
